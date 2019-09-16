2369ba722e1c67dd6e391438ff45a1e3e7c29624
PJ Hoberman <pj@twoparts.com>
2019-09-16 09:49:40 -0600

    html-ized

--- sw.js
+++ sw.js
@@ -1,39 +1,48 @@
-header = ["departure", "arrival", "duration", "stops", "fares"];
-console.log(header.join("\t"));
-printable_result = ""
+$(function() {
+  header = ["departure", "arrival", "duration", "stops", "fares"];
+  console.log(header.join("\t"));
+  printable_result = header.join("\t");
 
-$('li').each(function(e){
-  departure = $(this).find('.air-operations-time-status[type="origination"] span.time--value').text();
-  departure = departure.match("Departs (.*)")[1];
-  arrival = $(this).find('.air-operations-time-status[type="destination"] span.time--value').text();
-  arrival = arrival.match("Arrives (.*)")[1];
-  duration = $(this).find('.flight-stops--duration-time').text();
-  stops = $(this).find('.flight-stops--duration').text();
-  stops = stops.match(/.*(\d)\sstops?/)[1]
-  fare_els = $(this).find('.fare-button');
-  fares = [];
-  for(i=0; i<fare_els.length; i++){
-  	el = $(fare_els[i]);
-    this_fare = el.find('.fare-button--value-total').text();
-    low = el.find('.seats-left-text').text();
-    if(low !== "") {
-      fares.push(this_fare + " " + low);    
-    } else {
-      fares.push(this_fare);
+  $('button').click(function(){
+    html = $($('#input').val());
+    li = html.find('li');
+    li.each(function(e){
+      departure = $(this).find('.air-operations-time-status[type="origination"] span.time--value').text();
+      departure = departure.match("Departs (.*)")[1];
+      arrival = $(this).find('.air-operations-time-status[type="destination"] span.time--value').text();
+      arrival = arrival.match("Arrives (.*)")[1];
+      duration = $(this).find('.flight-stops--duration-time').text();
+      stops = $(this).find('.flight-stops--duration').text();
+      stops = stops.match(/.*(\d)\sstops?/)[1]
+      fare_els = $(this).find('.fare-button');
+      fares = [];
+      for(i=0; i<fare_els.length; i++){
+        el = $(fare_els[i]);
+        this_fare = el.find('.fare-button--value-total').text();
+        low = el.find('.seats-left-text').text();
+        if(low !== "") {
+          fares.push(this_fare + " " + low);    
+        } else {
+          fares.push(this_fare);
     } // else
   } // for
   result = {
-  	"departure": departure,
+    "departure": departure,
     "arrival": arrival,
     "duration": duration,
     "stops": stops,
     "fares": fares
   }
 
-	result = [departure, arrival, duration, stops, fares];
+  result = [departure, arrival, duration, stops, fares];
   console.log(result.join("\t"));
   printable_result += "\n" + result.join("\t");
 
   
 });
-console.log(printable_result);
+    console.log(printable_result);
+    $('#output').val(printable_result).focus().select();
+  });
+  
+});
+
